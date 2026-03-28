1e39a5515684c3f56518cdbf21bcbc8426df7dd8
Rishabh Malviya <malviyarishabh88@gmail.com>
2026-03-28 10:49:40 +0530

    Add Excel file and update scripts to read and save data

---
+++ app.py
@@ -0,0 +1,5 @@
+import pandas as pd 
+# read file from csv file into dataframe
+# df  = pd.read_csv("D:\pandas\krishna_soni.csv", encoding='latin-1')
+df = pd.read_excel("Topic_Wise_Level_Wise_LeetCode.xlsx")
+print(df)
---
+++ save.py
@@ -0,0 +1,11 @@
+import pandas as pd
+data = {
+    'Name': ['Alice', 'Bob', 'Charlie'],
+    'Age': [25, 30, 35],
+    'City': ['New York', 'San Francisco', 'Los Angeles']
+
+}
+df =pd.DataFrame(data)
+print(df)
+# save the dataframe to a csv file
+df.to_csv('output.csv', index=False)
