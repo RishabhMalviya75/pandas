2cf37c21edece1be0b628b6ba1c4f34ec3578b25
Rishabh Malviya <malviyarishabh88@gmail.com>
2026-03-28 22:34:40 +0530

    Add new scripts for data manipulation: create adding.py, removing.py, updatin2.py, and updating.py

---
+++ updatin2.py
@@ -0,0 +1,11 @@
+import pandas as pd
+data = {
+    'Name': ['Alice', 'Bob', 'Charlie', 'David', 'Eve', 'Frank', 'Grace', 'Heidi'],
+    'Age': [25, 30, 35, 40, 45, 50, 55, 60 ],
+    'Salary': [50000, 60000, 70000, 80000, 90000, 100000, 110000, 120000],
+    'performance-score': [85, 80, 75, 70, 65, 60, 55, 50]
+}
+
+df = pd.DataFrame(data)
+df['Salary'] = df['Salary'] * 1.05
+print(df)
---
+++ updating.py
@@ -0,0 +1,11 @@
+import pandas as pd
+data = {
+    'Name': ['Alice', 'Bob', 'Charlie', 'David', 'Eve', 'Frank', 'Grace', 'Heidi'],
+    'Age': [25, 30, 35, 40, 45, 50, 55, 60 ],
+    'Salary': [50000, 60000, 70000, 80000, 90000, 100000, 110000, 120000],
+    'performance-score': [85, 80, 75, 70, 65, 60, 55, 50]
+}
+
+df = pd.DataFrame(data)
+df.loc[1,"Age"]= 1234
+print(df)
